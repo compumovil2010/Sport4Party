3aed4034c8c65e849552857b6d79371a60b0036c
Santiago Chaparro <santy0710@hotmail.com>
2020-03-02 22:08:29 -0500

    vistas eventos, visualizar, editar, etc etc

--- app/src/main/java/com/example/sport4party/MainActivity.java
+++ app/src/main/java/com/example/sport4party/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.sport4party;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button crearEvento;
+    Button editarEvento;
+    Button infoEvento;
+    Button normal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        crearEvento=findViewById(R.id.crearEvento);
+        editarEvento=findViewById(R.id.editarEvento);
+        infoEvento= findViewById(R.id.verInfo);
+        normal=(Button)findViewById(R.id.PersonaNormal) ;
+        crearEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(), CrearEvento.class);
+                intent.putExtra("pantalla",1);
+                startActivity(intent);
+            }
+        }
+        );
+        editarEvento.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               Intent intent=new Intent(v.getContext(), CrearEvento.class);
+                                               intent.putExtra("pantalla",0);
+                                               startActivity(intent);
+                                           }
+                                       }
+        );
+        infoEvento.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Intent intent=new Intent(v.getContext(), InformacionEvento.class);
+                                                intent.putExtra("pantalla",0);
+                                                startActivity(intent);
+                                            }
+                                        }
+        );
+        normal.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              Intent intent=new Intent(v.getContext(), InformacionEvento.class);
+                                              intent.putExtra("pantalla",1);
+                                              startActivity(intent);
+                                          }
+                                      }
+        );
     }
 }
