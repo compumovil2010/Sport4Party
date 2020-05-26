588e593eb29219fb556ffa072f8077ea5b819974
barajas-d <diegobarajas102030@gmail.com>
2020-05-26 00:18:17 -0500

    Cositas extra

--- app/src/main/java/com/example/sport4party/InicioDeSesión.java
+++ app/src/main/java/com/example/sport4party/InicioDeSesión.java
@@ -20,6 +20,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.regex.Matcher;
@@ -30,6 +34,8 @@ public class InicioDeSesión extends AppCompatActivity{
     private FirebaseAuth mAuth;
     private EditText mUser;
     private EditText mPassword;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,7 @@ public class InicioDeSesión extends AppCompatActivity{
 
         mAuth = FirebaseAuth.getInstance();
 
+        database=FirebaseDatabase.getInstance();
         //progressDialog = new ProgressDialog(this);
 
     }
@@ -52,7 +59,7 @@ public class InicioDeSesión extends AppCompatActivity{
         updateUI(currentUser);
     }
 
-    private void updateUI(FirebaseUser currentUser){
+    private void updateUI(final FirebaseUser currentUser){
         if(currentUser!=null){
 
             //----------------- PRUEBAS FIREBASE
@@ -81,12 +88,31 @@ public class InicioDeSesión extends AppCompatActivity{
 
             //----------------------------------
 
-
-            Intent change = new Intent(this, Mapa.class);
-            change.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            change.putExtra("user", currentUser.getEmail());
-            startActivity(change);
-            finish();
+            myRef=database.getReference("Jugador");
+            myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot singleSnapShot : dataSnapshot.getChildren()) {
+                        if(singleSnapShot.getKey().equals(mAuth.getUid())){
+                            HashMap<String, Object> datos = (HashMap<String, Object>) singleSnapShot.getValue();
+                            if(datos.containsKey("tipo")){
+                                if(datos.get("tipo").equals("1")){
+                                    Intent change = new Intent(InicioDeSesión.this, Admin.class);
+                                    change.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(change);
+                                    finish();
+                                }
+                                else{
+                                    Intent change = new Intent(InicioDeSesión.this, Mapa.class);
+                                    change.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    change.putExtra("user", currentUser.getEmail());
+                                    startActivity(change);
+                                    finish();
+                                }
+                            }
+                        }
+                    }
+                }@Override public void onCancelled(@NonNull DatabaseError databaseError) { } });
         } else {
             //email.setText("");
             //password.setText("");
@@ -101,7 +127,6 @@ public class InicioDeSesión extends AppCompatActivity{
     public void toMapa(View v) {
 
         if(validateForm()){
-
             String email = mUser.getText().toString().trim();
             String contraseña = mPassword.getText().toString().trim();
 
